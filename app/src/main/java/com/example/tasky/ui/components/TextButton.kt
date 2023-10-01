7e23e0207a12a8e53408dd1b772cc2e1da0970a0
Alfred <alfredguimaz@pgatourhq.com>
2023-10-01 15:49:15 -0400

    feat(registration-screen)add a initial start with Login Screen

---
+++ app/src/main/java/com/example/tasky/ui/components/TextButton.kt
@@ -0,0 +1,53 @@
+package com.example.tasky.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun TextButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+    TextButton(
+        onClick = onClick,
+    ) {
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+    }
+}
+
+
+@Composable
+fun TextButton(
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            Color.Black,
+            contentColor = Color.White
+        ),
+        modifier = Modifier
+            .heightIn(65.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(percent = 50)
+    ) {
+        content()
+    }
+}
